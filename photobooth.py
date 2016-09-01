afb8730906a7fee091d3e3f0a6f80cc8c142d8fb
Brian Dickens <briandickens@gmail.com>
2016-09-01 12:42:39 -0400

    Made some changes to work better

--- photobooth.py
+++ photobooth.py
@@ -20,6 +20,19 @@ import io
 from PIL import Image, ImageOps
 import RPi.GPIO as GPIO
 
+HALF_WIDTH = 175
+HALF_HEIGHT = 200
+
+PHOTO_WIDTH = HALF_WIDTH * 2
+PHOTO_HEIGHT = HALF_HEIGHT * 2
+
+FOOTER_HEIGHT = 130
+BORDER_WIDTH = 10
+BG_COLOR = (255,255,255)
+
+PAGE_WIDTH = 1400
+PAGE_HEIGHT = 1800
+
 def takePhotosToFile(count):
     with picamera.PiCamera() as camera:
         camera.resolution = (1024, 768)
@@ -46,6 +59,21 @@ def takePhotosToPIL(count):
             time.sleep(2)
     return images
 
+def assemblePhotoSheet(images):
+    footer = Image.open('/home/pi/photobooth/resources/footer.jpg')
+    final_photo = Image.new('RGB', (((PHOTO_WIDTH * 2) + (BORDER_WIDTH * 3)), (PHOTO_HEIGHT * 2) + (BORDER_WIDTH * 3) + FOOTER_HEIGHT,), BG_COLOR)
+    for i in range(len(images)):
+        images[i] = ImageOps.fit(images[i], (PHOTO_WIDTH, PHOTO_HEIGHT), centering = (0.5, 0.5))
+        #images[i] = images[i].rotate(270)
+        images[i] = ImageOps.autocontrast(images[i], cutoff=0)
+
+    final_photo.paste(images[0], (BORDER_WIDTH, BORDER_WIDTH))
+    final_photo.paste(images[1], ((BORDER_WIDTH * 2 + PHOTO_WIDTH), BORDER_WIDTH))
+    final_photo.paste(images[2], (BORDER_WIDTH, ((BORDER_WIDTH * 2) + PHOTO_HEIGHT)))
+    final_photo.paste(images[3], ((BORDER_WIDTH * 2 + PHOTO_WIDTH), (BORDER_WIDTH * 2 + PHOTO_HEIGHT)))
+    final_photo.paste(footer, (BORDER_WIDTH, ((BORDER_WIDTH * 3) + (PHOTO_HEIGHT * 2)))) 
+    return images, final_photo
+
+
 if __name__ == '__main__':
     pass
-            
